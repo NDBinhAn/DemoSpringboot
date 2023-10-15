0a4e27bfbb6e0c10d85aa84bdcf44250e93c2644
Nguyen Truc Lan <ngtruclan2302@gmail.com>
2023-10-15 17:08:21 +0700

    Init Project by start.spring.io

---
+++ src/main/java/com/example/anndb/AnndbApplication.java
@@ -0,0 +1,13 @@
+package com.example.anndb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnndbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnndbApplication.class, args);
+	}
+
+}
